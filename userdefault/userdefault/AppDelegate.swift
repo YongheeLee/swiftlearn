f6c25f791dcfec7f549e00ea7ac8dd79a664bf31
yongheelee <yhhhlee@gmail.com>
2021-10-26 06:31:03 -0700

    notification

--- userdefault/userdefault/AppDelegate.swift
+++ userdefault/userdefault/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        if #available(iOS 10.0, *){
+            let noticenter = UNUserNotificationCenter.current()
+            noticenter.requestAuthorization(options: [.alert, .badge, .sound])
+            {(didAllow, e) in
+                
+            }
+        }else
+        {
+            
+        }
+        
         return true
     }
 
--- userdefault/userdefault/ViewController.swift
+++ userdefault/userdefault/ViewController.swift
@@ -18,6 +18,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func push(_ sender: Any) {
+        let alert = UIAlertController(title: "알람", message: "This is sampe", preferredStyle: UIAlertController.Style.alert)
+        let cancel = UIAlertAction(title: "default", style:UIAlertAction.Style.default, handler: {(_) in
+            self.rUpdate.text = alert.textFields?[0].text
+        })
+        let cancel1 = UIAlertAction(title: "cancel", style:UIAlertAction.Style.cancel)
+        { (_) in
+            self.rEmail.text = "cancel button was pushed"
+        }
+        
+        let cancel2 = UIAlertAction(title: "destructive", style:UIAlertAction.Style.destructive, handler: {(_)in
+            self.rPeriod.text = "desctruc button was pushed"
+        })
+        
+        alert.addTextField(configurationHandler: {(tf) in
+            
+        })
+        
+        alert.addAction(cancel)
+        alert.addAction(cancel1)
+        alert.addAction(cancel2)
+        self.present(alert, animated: false)
+        
+    }
     override func viewWillAppear(_ animated: Bool) {
         let ud = UserDefaults.standard
         
@@ -33,4 +57,5 @@ class ViewController: UIViewController {
         rPeriod.text = "\(Int(interval))each"
     }
 }
+
 
